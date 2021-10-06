3ed172731e4e3eb8b63c845c7008e7fba242df7f
“vividxxxxx” <vividxxxx@gmail.com>
2021-10-06 18:00:54 +0900

    add search bar

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,28 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String? text;
+
     return MaterialApp(
-      home:Scaffold(
-        appBar: AppBar(
-          title: const Text("SunBay"),
-        ),
-        body: const Center(
-          child: Text("Lets build app")
-        ),
-      )
-    );
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text("SunBay"),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            onChanged: (value) {
+              text = value;
+            },
+          ),
+          ElevatedButton.icon(
+              icon: const Icon(Icons.search_outlined),
+              onPressed: () {
+                print(text);
+              },
+              label: const Text("Search"))
+        ],
+      ),
+    ));
   }
 }
